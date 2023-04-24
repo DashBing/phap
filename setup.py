c19acff23de584d6cb62241d770524532141240e
DashBing <mcbbkf@outlook.com>
2023-04-24 12:27:46 +0800

    20230424-关于版本号的二次修改

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="stralgo",
-    version="0.1.2b1",  #版本
+    version="1.1.0b1",  #版本
     author="DashBing",
     author_email="mcbbkf@outlook.com",
     description="A Python library for various string algorithms",
