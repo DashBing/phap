0e659951f03fe8ae8a4199675e268babf0bf1f98
DashBing <mcbbkf@outlook.com>
2023-07-01 22:32:24 +0800

    稍微做修改treenode

---
+++ src/algo/treenode.py
@@ -0,0 +1,9 @@
+class treenode:
+    left = 0
+    right = 0
+    __i = "<class 'int'>"
+    def __init__(self, left = 0, right = 0):
+        self.left = left
+        self.right = right
+    def __str__(self) -> str:
+        pass
