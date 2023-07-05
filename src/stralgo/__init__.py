1f37897401e177f95e6e1f7c4d6967f36a0e3c1e
DashBing <mcbbkf@outlook.com>
2023-07-05 18:50:36 +0800

    添加小更改

--- src/stralgo/__init__.py
+++ src/stralgo/__init__.py
@@ -3,3 +3,13 @@ __all__ = [
     "json",
     "hash",
 ]
+
+def hex_to_int(text:str) -> int:
+    odata = 0; text = text.upper();
+    for c in text:
+        tmp = ord(c)
+        if tmp <= ord('9'):
+            odata = odata << 4; odata += tmp - ord('0')
+        elif ord('A') <= tmp <= ord('F'):
+            odata = odata << 4; odata += tmp - ord('A') + 10
+    return odata
